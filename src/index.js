e09156e1446d2be7aa040f93dcc98d4fa583bdc1
Kenneth <gilmorekenneth@gmail.com>
2020-05-04 17:26:44 -0400

    basic layout done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import './style.css';
 import './reset.css';
 
-alert("hi")
+const main = document.querySelector('#content');
+const dashboard = document.querySelector("#dashboard");
+const upcoming = document.querySelector('#upcoming');
+const urgent = document.querySelector('#urgent');
+const newProject = document.querySelector('#new-project');
+const newTask = document.querySelector('#new-task');
+const header = document.querySelector('#header');
 
-const main = (() => {
+const taskMaker = (title, description, dueDate, priority) => {
+  const sayHello = () => { console.log("hello") };
+  return {title, description, dueDate, priority, sayHello};
+};
   
-  const taskMaker = (title, description, dueDate, priority) => {
-    const sayHello = () => { console.log("hello") };
-    return {title, description, dueDate, priority, sayHello};
-  };
-  
-  return {taskMaker};
-})();
+const elementMaker = (type, className, text, place) => {
+  let element = document.createElement(type);
+  element.setAttribute('class', className);
+  element.textContent = text;
+  place.appendChild(element);
+}
 
+const eraseContent = (element) => {
+  element.innerHTML = "";
+}
 
+const makeHeading = (heading) => {
+  header.textContent = heading;
+}
+
+const makeUpcoming = () => {
+  makeHeading("Upcoming")
+}
+
+const makeUrgent = () => {
+  makeHeading("Urgent")
+}
+
+const makeDashboard = () => {
+  makeHeading("Dashboard")
+};
+
+/* event listeners */
+dashboard.addEventListener('click', makeDashboard);
+upcoming.addEventListener('click', makeUpcoming); 
+urgent.addEventListener('click', makeUrgent);
+
+makeDashboard();
 
